d0f897c22e019b29d740c5dec764af9080cde602
EmploymentExchange <employmentexchange406@gmail.com>
2025-08-04 11:12:33 +0530

    Initial commit of Flask form app

--- flask_app.py
+++ flask_app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 
 # Google Sheets setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("flaskformdataproject-38167ba1ba59.json", scope)
-#creds_json = base64.b64decode(os.environ["GOOGLE_CREDS"]).decode("utf-8")
-#creds_dict = json.loads(creds_json)
-#creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+#creds = ServiceAccountCredentials.from_json_keyfile_name("flaskformdataproject-38167ba1ba59.json", scope)
+creds_json = base64.b64decode(os.environ["GOOGLE_CREDS"]).decode("utf-8")
+creds_dict = json.loads(creds_json)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open("Placement_Form_Responses").sheet1
 
